4131cdcfe66f88e97a02f71256cc66b1da404d48
gercogornounlar <105032150+gercogornounlar@users.noreply.github.com>
2023-07-04 22:04:57 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let textArea = document.querySelector(".text-area")
+let message = document.querySelector('.message')
+let copyBtn = document.querySelector(".copy");
+// copyBtn.style.display = "none"
+
+
+
+/*  La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncrypt(){
+  const textEncrypted = encrypt(textArea.value);
+  message.value = textEncrypted;
+  textArea.value = '';
+  message.style.backgroundImage = 'none';
+}
+
+function btnDecrypt(){
+  const textDecrypted = decrypt(textArea.value);
+  message.value = textDecrypted;
+  textArea.value = '';
+  message.style.backgroundImage = 'none';
+}
+
+function encrypt(stringEncrypted){
+  let arrayCode = [
+    ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncrypted = stringEncrypted.toLowerCase()
+
+    for (let i = 0; i < arrayCode.length; i++) {
+      if (stringEncrypted.includes(arrayCode[i][0])) {
+        stringEncrypted = stringEncrypted.replaceAll(arrayCode[i][0], arrayCode[i][1])
+      }
+    }
+    return stringEncrypted
+}
+
+function decrypt(stringDecrypted){
+  let arrayCode = [
+    ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDecrypted = stringDecrypted.toLowerCase()
+
+    for (let i = 0; i < arrayCode.length; i++) {
+      if (stringDecrypted.includes(arrayCode[i][1])) {
+        stringDecrypted = stringDecrypted.replaceAll(arrayCode[i][1], arrayCode[i][0])
+      }
+    }
+    return stringDecrypted
+}
+
+function copyTextToClip(){
+  message.select();
+  navigator.clipboard.writeText(message.value)
+  message.value = "";
+  alert("Copied to Clipboard")
+}
